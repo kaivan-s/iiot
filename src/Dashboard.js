1a4708b8547cb43da1d60ed6b1e664068abbff72
kaivan <kaivanshah1663@gmail.com>
2024-02-05 13:22:01 +0530

    Changed clientID

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -63,7 +63,7 @@ const Dashboard = () => {
 
   useEffect(() => {
     const client = mqtt.connect('ws://w1900034.emqx.cloud:8083/mqtt', {
-      clientId: process.env.REACT_APP_CLIENT_ID,
+      clientId: process.env.REACT_APP_CLIENT_ID+ "-app",
       username: process.env.REACT_APP_USERNAME,
       password: process.env.REACT_APP_PASSWORD,
     });
